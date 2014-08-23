500ad68e0fac701035c9bd50f51f2a953b6c61bd
rgbm21 <Abul.MidhatHasanRaghib@metropolia.fi>
2014-08-23 17:58:21 +0300

    init

---
+++ CatchAMovie/js/App.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../Scripts/typings/jquery/jquery.d.ts" />
+
+var AppHandler = (function () {
+    function AppHandler(moviesJson) {
+        this._movies = moviesJson;
+    }
+    AppHandler.prototype.GetMovie = function (elementAt) {
+        var movie = this._movies[elementAt];
+        return movie;
+    };
+
+    AppHandler.prototype.GetAllMovies = function () {
+        var movies = this._movies;
+        return movies;
+    };
+    return AppHandler;
+})();
+//# sourceMappingURL=App.js.map
